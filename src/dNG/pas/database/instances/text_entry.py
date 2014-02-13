56076a3c0a892065cfbcb4a0e9bf79553b780075
@4u <web@direct-netware.de>
2014-02-13 08:54:37 +0100

    Initial commit

---
+++ src/dNG/pas/database/instances/text_entry.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+##j## BOF
+
+"""
+dNG.pas.database.instances.TextEntry
+"""
+"""n// NOTE
+----------------------------------------------------------------------------
+direct PAS
+Python Application Services
+----------------------------------------------------------------------------
+(C) direct Netware Group - All rights reserved
+http://www.direct-netware.de/redirect.py?pas;http;contentor
+
+The following license agreement remains valid unless any additions or
+changes are being made by direct Netware Group in a written form.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc.,
+59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+----------------------------------------------------------------------------
+http://www.direct-netware.de/redirect.py?licenses;gpl
+----------------------------------------------------------------------------
+#echo(pasHttpContentorVersion)#
+#echo(__FILEPATH__)#
+----------------------------------------------------------------------------
+NOTE_END //n"""
+
+from sqlalchemy import Column, TEXT, VARCHAR
+
+from .abstract import Abstract
+
+class TextEntry(Abstract):
+#
+	"""
+"TextEntry" contains the database representation for a text entry.
+
+:author:     direct Netware Group
+:copyright:  direct Netware Group - All rights reserved
+:package:    pas
+:subpackage: database_text
+:since:      v0.1.00
+:license:    http://www.direct-netware.de/redirect.py?licenses;gpl
+             GNU General Public License 2
+	"""
+
+	__tablename__ = "{0}_text_entry".format(Abstract.get_table_prefix())
+	"""
+SQLAlchemy table name
+	"""
+
+	id = Column(VARCHAR(32), primary_key = True)
+	"""
+text_entry.id
+	"""
+	content = Column(TEXT, index = True)
+	"""
+text_entry.content
+	"""
+#
+
+##j## EOF
---
+++ src/dNG/pas/database/instances/text_mixin.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+##j## BOF
+
+"""
+dNG.pas.database.instances.AbstractText
+"""
+"""n// NOTE
+----------------------------------------------------------------------------
+direct PAS
+Python Application Services
+----------------------------------------------------------------------------
+(C) direct Netware Group - All rights reserved
+http://www.direct-netware.de/redirect.py?pas;http;contentor
+
+The following license agreement remains valid unless any additions or
+changes are being made by direct Netware Group in a written form.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the
+Free Software Foundation; either version 2 of the License, or (at your
+option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc.,
+59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+----------------------------------------------------------------------------
+http://www.direct-netware.de/redirect.py?licenses;gpl
+----------------------------------------------------------------------------
+#echo(pasHttpContentorVersion)#
+#echo(__FILEPATH__)#
+----------------------------------------------------------------------------
+NOTE_END //n"""
+
+from sqlalchemy.ext.declarative import declared_attr
+from sqlalchemy.orm import foreign, relationship, remote
+
+from .text_entry import TextEntry
+
+class TextMixin(object):
+#
+	"""
+"TextMixin" provides a relationship to an text entry in the database based
+on the object ID.
+
+:author:     direct Netware Group
+:copyright:  direct Netware Group - All rights reserved
+:package:    pas
+:subpackage: database_text
+:since:      v0.1.00
+:license:    http://www.direct-netware.de/redirect.py?licenses;gpl
+             GNU General Public License 2
+	"""
+
+	@declared_attr
+	def rel_text_entry(self):
+	#
+		"""
+Relation to TextEntry (backref is set as "rel_referer")
+
+:return: (object) SQLalchemy relationship description
+:since:  v0.1.00
+		"""
+
+		return relationship(TextEntry, backref = "rel_referer", primaryjoin = (foreign(self.id_object) == remote(TextEntry.id)), uselist = False)
+	#
+#
+
+##j## EOF
